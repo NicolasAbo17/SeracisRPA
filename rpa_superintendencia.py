8e8d8d67195b9a98c87db45734aa2dce17b6043f
Nicolás Abondano <nf.abondano@uniandes.edu.co>
2022-10-26 15:16:13 -0500

    First version

--- rpa_superintendencia.py
+++ rpa_superintendencia.py
@@ -1,4 +1,6 @@
 # Librerias
+from trace import Trace
+from xmlrpc.client import boolean
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,6 +11,7 @@ import warnings
 import time
 import pandas as pd
 import openpyxl
+import xlrd
 import os
 import shutil
 import datetime
@@ -48,10 +51,14 @@ empleadosArchivo = "empleados.xlsx"
 empleadosColumnas = ['NOMBRE', 'IDENTIFICACIÓN', 'FECHA_DESDE', 'CARGO', 'ZONA', 'SUBZONA']
 empleadosCargos = ['ESCOLTA', 'MANEJADOR CANINO', 'OPERADOR (A) MEDIOS TECNOLOGICOS', 'SUPERVISOR', 'SUPERVISOR CANINO', 'SUPERVISOR CENTRAL DE MONITOREO', 'SUPERVISOR DE CONTROL DE COMUNICACIONES', 'SUPERVISOR DE PORTERIA', 'SUPERVISOR DE ZONA', 'VIGILANTE']
 
-
+global loginBool
+loginBool = False
 
 # Functions
 def descargarSupervigilancia(url): 
+    global loginBool
+    loginBool = False
+
     driver.get(url)
 
     a = driver.find_element(By.ID, 'ctl00_contentMaster_TxNit')
@@ -65,18 +72,34 @@ def descargarSupervigilancia(url):
 
     time.sleep(5)
 
-def leerSupervigilancia(filename):
-    return pd.read_excel(directorioDes + "/" + filename, skiprows=[0])
+def leerUltimo(skipRows, delete, xls):
+    old_file = max([directorioDes +'\\'+ f for f in os.listdir(directorioDes)], key=os.path.getctime)
+    if xls:
+        workbook = xlrd.open_workbook_xls(old_file, ignore_workbook_corruption=True)  
+        df = pd.read_excel(workbook)
+    elif skipRows:
+        df = pd.read_excel(old_file, skiprows=[0])
+    else:
+        df = pd.read_excel(old_file)
+    if delete:
+        os.remove(old_file)
+    return df
+
+def leerSupervigilancia():
+    return leerUltimo(True, False, False)
 
 def loginSemantica():
-    log_sem_1 = driver.find_element(By.NAME, '_username')
-    log_sem_1.send_keys(usuarioSemantica)
+    global loginBool
+    if loginBool == False:
+        loginBool = True
+        log_sem_1 = driver.find_element(By.NAME, '_username')
+        log_sem_1.send_keys(usuarioSemantica)
 
-    log_sem_2 = driver.find_element(By.NAME, '_password')
-    log_sem_2.send_keys(usuarioSemantica)
+        log_sem_2 = driver.find_element(By.NAME, '_password')
+        log_sem_2.send_keys(usuarioSemantica)
 
-    btn_log = driver.find_element(By.ID, 'js-login-btn')
-    btn_log.click()
+        btn_log = driver.find_element(By.ID, 'js-login-btn')
+        btn_log.click()
 
 def subirListaAcreditados():
     driver.get(subirAcreditacionUrl)
@@ -101,6 +124,9 @@ def cargarArchivoNuevaVentana(toClick, filepath, writeNum):
     if writeNum: 
         txt_numero = driver.find_element(By.ID, 'form_numero')
         txt_numero.send_keys('10')
+    else:
+        cbx_validacion = driver.find_element(By.ID, 'form_omitirValidacion')
+        cbx_validacion.click()
 
     btn_browse = driver.find_element(By.ID, 'form_attachment')
     btn_browse.send_keys(filepath)
@@ -110,6 +136,7 @@ def cargarArchivoNuevaVentana(toClick, filepath, writeNum):
 
     wait.until(EC.number_of_windows_to_be(1))
     driver.switch_to.window(original_window)
+    os.remove(filepath)
 
 def descargarEmpleadosSemantica():
     driver.get(empleadosUrl)
@@ -132,20 +159,20 @@ def descargarEmpleadosSemantica():
     time.sleep(8)
 
 def leerEmpleados():
-    df = pd.read_excel(directorioDes + "/" + empleadosArchivo)
+    df = leerUltimo(False, True, False)
     df.drop(df.columns.difference(empleadosColumnas), 1, inplace=True)
     df = df[df['CARGO'].isin(empleadosCargos)]
     df.loc[df['CARGO'] == empleadosCargos[2], 'CARGO'] = 'OPERADOR DE MEDIOS TECNOLOGICOS'
     df['FECHA.ACR'] = 'Na'
     return df.sort_values(by=['IDENTIFICACIÓN', 'CARGO'], ascending=[True, True])
 
-def aniadirFecha(df1, df2):
+def aniadirFecha(df1, df2, col):
     i = 0
     j = 0
     while i < df1.shape[0] and j < df2.shape[0]:
         if df1.loc[df1.index[i],'IDENTIFICACIÓN'] == df2.loc[df2.index[j],'IdNum']:
             if df1.loc[df1.index[i],'CARGO'].strip() == df2.loc[df2.index[j],'Cargo'].strip():
-                df1.loc[df1.index[i],'FECHA.ACR'] = df2.loc[df2.index[j],'Vigen.Acr']
+                df1.loc[df1.index[i],'FECHA.ACR'] = df2.loc[df2.index[j],col]
                 i += 1
             j += 1
         else:
@@ -155,31 +182,64 @@ def aniadirFecha(df1, df2):
                 j+=1
     return df1
 
+def descargarApos():
+    driver.get(subirAcreditacionUrl)
+    loginSemantica()
 
+    informe_completo = pd.DataFrame()
+    # for i in range(0,len(empleados.index)):
+    for i in range(0,len(empleados.index)):
+        iden = str(empleados.loc[i,['IDENTIFICACIÓN']].item())
+        
+        empl_bus = driver.find_element(By.ID, 'form_numeroIdentificacion')
+        empl_bus.clear()
+        empl_bus.send_keys(iden)
 
-# Process
-# Descarga de supervigilancia las personas en proceso de acreditación y ya acreditadas
-descargarSupervigilancia(acreditadosUrl)
-acreditados = leerSupervigilancia(acreditadosArchivo)
-acreditados = acreditados.sort_values(by=['IdNum', 'Cargo'], ascending=[True, True])
+        empl_filtro = driver.find_element(By.ID, 'form_btnFiltro')
+        empl_filtro.click()
+        time.sleep(2)
 
-descargarSupervigilancia(enprocesoUrl)
-enproceso = leerSupervigilancia(enprocesoArchivo)
-enproceso = enproceso.sort_values(by=['IdNum', 'Cargo'], ascending=[True, True])
+        btn_archivos = driver.find_element(By.ID, 'archivos')
+        btn_archivos.click()
+        btn_informe = driver.find_element(By.ID, 'form_btnInformeApo')
+        btn_informe.click()
+        time.sleep(2)
 
-subirListaAcreditados()
+        filename = max([directorioDes +'\\'+ f for f in os.listdir(directorioDes)], key=os.path.getctime)
+        print(filename)
 
-# Descarga los empleados como están registrados actualmente en semantica
-descargarEmpleadosSemantica()
-empleados = leerEmpleados()
+        informe_apo = leerUltimo(False, True, True)
+        informe_completo = pd.concat([informe_completo, informe_apo], ignore_index=True)
+        
+    print(informe_completo.head())
 
-# Añade la fecha de acreditación a quienes la tengan
-empleados = aniadirFecha(empleados, enproceso)
-empleados = aniadirFecha(empleados, acreditados)
+# # Process
+# # Descarga de supervigilancia las personas en proceso de acreditación y ya acreditadas
+# descargarSupervigilancia(acreditadosUrl)
+# acreditados = leerSupervigilancia()
+# acreditados = acreditados.sort_values(by=['IdNum', 'Cargo'], ascending=[True, True])
 
+# descargarSupervigilancia(enprocesoUrl)
+# enproceso = leerSupervigilancia()
+# enproceso = enproceso.sort_values(by=['IdNum', 'Cargo'], ascending=[True, True])
+
+# subirListaAcreditados()
+
+# # Descarga los empleados como están registrados actualmente en semantica
+# descargarEmpleadosSemantica()
+# empleados = leerEmpleados()
+
+# # Añade la fecha de acreditación a quienes la tengan
+# empleados = aniadirFecha(empleados, enproceso, 'Estado')
+# empleados = aniadirFecha(empleados, acreditados, 'Vigen.Acr')
+
+# empleados = empleados[empleados['FECHA.ACR'] == 'Na']
 # fec_hoy = datetime.now()
 # fec_hoy = fec_hoy.strftime('%d_%m_%Y_%H_%M')
 # new_file = directorioDes + "\Acreditados" + fec_hoy + ".xlsx"
 # empleados.to_excel(new_file, index=False, header=True)
+
+empleados = pd.read_excel(directorioDes + "\Acreditados26_10_2022_14_30.xlsx")
+descargarApos()
 
 driver.quit()
